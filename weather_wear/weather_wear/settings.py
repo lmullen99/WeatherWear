0af986104436f68f40664a9c46e2c5b6e84f066a
lmullen99 <72515842+lmullen99@users.noreply.github.com>
2021-11-18 14:13:44 -0600

    adding login page

--- weather_wear/weather_wear/settings.py
+++ weather_wear/weather_wear/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 import os.path
 from pathlib import Path
 
@@ -55,7 +56,8 @@ ROOT_URLCONF = 'weather_wear.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR.joinpath('templates'))],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+      # 'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
