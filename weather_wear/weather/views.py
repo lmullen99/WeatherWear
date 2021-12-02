9165b46edf0aa789faec6c42e509d62cf496b43b
lmullen99 <72515842+lmullen99@users.noreply.github.com>
2021-12-01 23:53:27 -0600

    adds error-handling

--- weather_wear/weather/views.py
+++ weather_wear/weather/views.py
@@ -10,6 +10,8 @@ from django.views.generic import TemplateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import redirect
+from django.contrib import messages
 
 class AboutView(TemplateView):
     template_name = "about.html"
@@ -48,45 +50,58 @@ def delete(request):
         city = request.POST['pk']
         City.objects.get(name = city).delete()
         #return render(request, views.index)
+        messages.info(request, "Deletion successful.")
         return HttpResponseRedirect('index')
     else:
-        return HttpResponse("Deletion failed ")
+        messages.error(request, "Deletion unsuccessful.")
+        return HttpResponseRedirect('index')
 
 def index(request):
     api_id = '845b66b8bb799526bfe36f2e6c41589b'
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=845b66b8bb799526bfe36f2e6c41589b'
     # call the API on all the user's favorite cities
-    curr_user = request.user
-    print("current user is:")
-    print(curr_user)
-   # cities = City.objects.all()
     cities = City.objects.all().filter(owner=request.user)
 
     if request.method == 'POST':
         form = CityForm(request.POST)                    # create a City instance with the entered city name
-        if form.is_valid():                                        # for adding error handling
+        if form.is_valid():                              # add error handling
             instance = form.save(commit=False)
+            if True in [char.isdigit() for char in instance.name]:
+                messages.error(request, "City name must contain only letters")
+                return redirect('index')
+            city_weather = requests.get(url.format(instance.name)).json()
+            if city_weather['cod'] == "404":
+                messages.error(request, "City not found.")
+                return redirect('index')
             instance.name=instance.name.lower()
-            # print(instance.name) #for debug
             instance.owner = request.user
             instance.save()
-            #form.save()                                                 # add this to the sqlite City database
+
+            print("INStance saved")             # debug
     form = CityForm()
     weather_data = []
-    for city in cities:
-        #request JSON data and converts to python data type
-        city_weather = requests.get(url.format(city)).json()
-        #created the weather variable
-        weather = {
-            'city' : city,
-            'temperature' : city_weather['main']['temp'],
-            'humidity' : city_weather['main']['humidity'],
-            'description' : city_weather['weather'][0]['description'],
-            'wind': city_weather['wind']['speed'],
-            'icon' : city_weather['weather'][0]['icon']
-        }
-        weather_data.append(weather)
 
+    for city in cities:
+            #request JSON data and converts to python data type
+            city_weather = requests.get(url.format(city)).json()
+            """
+            # code should NEVER arrive here
+            if city_weather['cod'] == "404":
+                messages.error(request, "City not found.")
+                return redirect('index')
+            """
+
+
+            #created the weather variable
+            weather = {
+                'city' : city,
+                'temperature' : city_weather['main']['temp'],
+                'humidity' : city_weather['main']['humidity'],
+                'description' : city_weather['weather'][0]['description'],
+                'wind': city_weather['wind']['speed'],
+                'icon' : city_weather['weather'][0]['icon']
+            }
+            weather_data.append(weather)
     context = {'weather_data' : weather_data, 'form' : form}
 
     with sql.connect("weather/OUTFITS.db") as con:
@@ -95,5 +110,6 @@ def index(request):
         cur.execute(sql_select_query)
         con.commit()
     con.close()
+
 
     return render(request, 'weather/index.html', context)
