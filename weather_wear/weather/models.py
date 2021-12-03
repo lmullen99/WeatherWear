7dfbe8d0ad4c15be140febfd938cbc41f8afca7c
lmullen99 <72515842+lmullen99@users.noreply.github.com>
2021-12-03 11:23:44 -0600

    fixes outerwear in the table

--- weather_wear/weather/models.py
+++ weather_wear/weather/models.py
@@ -20,6 +20,6 @@ class Outfit(models.Model):
     temp = models.IntegerField(unique = True)
     top = models.CharField(max_length=40)
     bottom = models.CharField(max_length=40)
-    outerwear = models.CharField(max_length=40)
+    outerwear = models.CharField(max_length=40, null=True, blank=True)
     accessories = models.CharField(max_length=50)
     footwear = models.CharField(max_length=40)
